183169bd05cce2e2c9a4f3854764ca2b32e2ba28
jh_desk <steady051@naver.com>
2024-06-21 15:29:51 +0900

    setTimeout 테스트

---
+++ test.js
@@ -0,0 +1,13 @@
+setTimeout(() => {
+    console.log("setTimeout")
+}, 0)
+
+setImmediate(() => {
+    console.log("setImmediate")}
+)
+
+setInterval(()=> {
+    console.log("setInterval")
+}, 1000)
+
+console.log("야")
